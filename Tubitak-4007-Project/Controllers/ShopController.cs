3477480adb62673e558367962a1030b73c9a9e60
yusufemre6 <95129061+yusufemre6@users.noreply.github.com>
2024-08-03 16:26:59 +0300

    Header was created

---
+++ Tubitak-4007-Project/Controllers/ShopController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tubitak_4007_Project.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
